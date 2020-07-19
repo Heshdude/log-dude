badb5e6b389a8a90f6cdbbf3cd3bfab8cacebd45
Heshdude <heshan.geasman@gmail.com>
2020-07-19 16:47:16 +1000

    Adding Minified logger

---
+++ logger.min.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+RESET='\033[0m';RED='\033[0;31m';GREEN='\033[0;32m';YELLOW='\033[0;33m';log () { echo "[`date "+%Y.%m.%d-%H:%M:%S%Z"`] $1 $2"; };info () { log "${GREEN}INFO$RESET $1"; };warn () { log "${YELLOW}WARN$RESET $1"; };error () { log "${RED}ERROR$RESET $1"; };
